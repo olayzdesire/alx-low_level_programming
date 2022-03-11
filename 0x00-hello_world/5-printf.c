bcb2bd63a92151b57adb60c4f2d4480ffbe54fd1
olayzdesire <olayzdesire@gmail.com>
2022-03-10 19:43:48 -0800

    0x00-hello_world_Task5_v5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints out string with printf
+ * main - prints out a string with printf
  * Return: Always 0(success)
- * Description: uses the main function to print a string
+ * Description: uses the main function to print out a string
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0); 
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
 }
