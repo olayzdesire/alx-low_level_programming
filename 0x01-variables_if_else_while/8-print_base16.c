88e8dad2ca3667215948e9eea996cb4ade8145c6
olayzdesire <olayzdesire@gmail.com>
2022-03-11 20:32:17 -0800

    0x01-variables_if_else_while_Task8_v1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers in base 16 in lowercase
+ * Description: using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char ch;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
