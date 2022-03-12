1205fb71501e237d496b563273fed991fe5bc130
olayzdesire <olayzdesire@gmail.com>
2022-03-11 18:59:40 -0800

    0x01-variables_if_else_while_Task2_v1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ * Description: using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char char0
+
+	for (ch0 = 'a'; ch0 <= 'z'; ch0++)
+	{
+		putchar(ch0);
+	}
+	putchar('\n');
+	return (0);
+}
