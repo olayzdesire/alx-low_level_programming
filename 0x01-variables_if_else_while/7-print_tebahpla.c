8257f6ed24d22087d19d606d3444fd17de6e9f42
olayzdesire <olayzdesire@gmail.com>
2022-03-11 20:13:34 -0800

    0x01-variables_if_else_while_Task7_v1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  prints all alphabets in reverse 
+ * Description: using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char ch0;
+
+	for (ch0 = 'z'; ch0 >= 'a'; ch0--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,4 +4,5 @@ Task2:
 Task3: 3-print_alphabets.c
 Task4: 4-print_alphabt.c
 Task5: 5-print_numbers.c  
-Task6: 6-print_numberz.c  
+Task6: 6-print_numberz.c
+Task7: 7-print_tebahpla.c  
