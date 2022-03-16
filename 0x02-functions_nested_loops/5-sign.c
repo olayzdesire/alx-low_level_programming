c1b8f99b99db21b3b8d6dd9ede5a43ed0fda26ae
olayzdesire <olayzdesire@gmail.com>
2022-03-15 21:57:13 -0700

    0x02-functions_Task5_v6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,13 @@
 
 int print_sign(int n)
 {
-       	if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
-	{
-	       	_putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
