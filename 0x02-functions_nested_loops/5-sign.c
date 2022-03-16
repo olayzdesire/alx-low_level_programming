9e69f77ed59dd4cd4d172a7e737c895b64946cb6
olayzdesire <olayzdesire@gmail.com>
2022-03-15 21:38:17 -0700

    0x02-functions_Task5_v3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,12 @@
 #include "main.h"
- /**
-  *print_sign - Print the sign of a number
-  *@n: type int integer, can be negative or positive
-  *Description: print +, 0, or - depending on number, along with return
-  *Return: 1 if +, 0 if 0, and -1 if -
-  */
+
+/**
+ * print_sign - Print the sign of a number
+ * @n: type int integer, can be negative or positive
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if + and greater than 0, 0 if 0, and -1 if - and less than 0
+ */
+
 int print_sign(int n)
 {
   if (n > 0)
