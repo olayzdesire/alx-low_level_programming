3cd93d7bb69d1880d4418a14bf5833cb658c5ca8
olayzdesire <olayzdesire@gmail.com>
2022-03-15 20:13:46 -0700

    0x02-functions_Task2_v2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@ void print_alphabet_x10(void)
 {
 		int i;
 		char ch;
-
-		i = 0;	
-		while (i < 10)
-		{	
+	
+		i = 0;
+	    	while (i < 10)
+		{
 			ch = 'a';											while (ch <= 'z')
-			{	
+			{
 				_putchar(ch);
 				ch++;
-			} 
+			}
 			_putchar('\n');
 			i++;
 		}
