3e8a45445cd018fe6775addfb495293c3b376278
olayzdesire <olayzdesire@gmail.com>
2022-03-16 03:13:49 -0700

    0x02-functions_Task6_v2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,12 +6,12 @@
  */
 int _abs(int n)
 {
-  if (n < 0)
-    {
-      return (n * -1);
-    }
-  else
-    {
-      return (n);
-    }
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
 }
