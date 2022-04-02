af9dc611115f29b24e6277b4f6618f198a6f1e44
olayzdesire <olayzdesire@gmail.com>
2022-04-02 14:17:11 -0700

    0x09-static_libraries_Task_1

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if lowecase 0 otherwise
+ * @c: Variable to use
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - if true, returns lowercase or uppercase letter, zero otherwise.
+ * @c: Variable to use
+ * Return: 1, if true 0 otherwise
+ *
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
