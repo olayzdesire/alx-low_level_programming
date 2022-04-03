78d16b106d3f14175ddc2f2f649cde1f8375b34a
olayzdesire <olayzdesire@gmail.com>
2022-04-03 15:47:22 -0700

    0x0A-argc_argv_Task0_v5

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  * @argv: prints an array of argument.
  * Return: 0, success.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
