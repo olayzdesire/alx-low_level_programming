079f256b33725df445d0329ae595c9a6d8fd63a5
olayzdesire <olayzdesire@gmail.com>
2022-04-03 16:05:50 -0700

    0x0A-argc_argv_Task0_v8

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include "stdio.h"
 
 /**
- * main - prints its name, followed by a new line.
- * @argc: prints the number of argument.
- * @argv: prints an array of argument.
- * Return: 0, success.
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int main(int argc, char argv[])
+
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
