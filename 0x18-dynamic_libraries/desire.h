c82d11bac074fcdaf02e3bfa5314d96537d2a281
olayzdesire <olayzdesire@gmail.com>
2022-06-18 09:26:25 -0700

    task 100

---
+++ 0x18-dynamic_libraries/desire.h
@@ -0,0 +1,12 @@
+#ifndef _DESIRE_H_
+#define _DESIRE_H_
+
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif /* _DESIRE_H_ */
---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,30 @@
+#include "desire.h"
+/**
+ * add, sub, mul, mod, div - functions in the 100-operations.so library
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
